111ce47675eb1fdb72a3f66220e2df6397409037
Jeff Lewis <474547+halldorr@users.noreply.github.com>
2019-08-17 20:53:25 -0400

    Add basic routing

--- Core/Router.php
+++ Core/Router.php
@@ -2,5 +2,41 @@
 
 class Router
 {
-	//code
+	//Array of routes
+	protected $routes = [];
+	//Params from matched route
+	protected $params = [];
+
+	//Add a route to the routing table
+	public function add($route, $params)
+	{
+		$this->routes[$route] = $params;
+	}
+
+	//Get all routes
+	public function getRoutes()
+	{
+		return $this->routes;
+	}
+
+	//match the routes in the routing table, setting params property if route found
+	public function match($url)
+	{
+		foreach ($this->routes as $route => $params)
+		{
+			if ($url == $route)
+			{
+				$this->params = $params;
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	//get currently matched parameters
+	public function getParams()
+	{
+		$this->params;
+	}
 }
